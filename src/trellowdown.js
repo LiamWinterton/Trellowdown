5d311399ab60a0c6ebfdc8867f00547a4b2b2130
Unknown <liam.winterton.dev@gmail.com>
2018-04-10 16:35:44 +0100

    Whooooo

--- src/trellowdown.js
+++ src/trellowdown.js
@@ -53,8 +53,31 @@ export class Trellowdown {
         let html = '<div class="trellowdown">'
 
         boards.forEach(board => {
-            html += '<div class="board">'
+            let boardTimestamp = board.id.substring(0,8)
+            let boardDate = new Date(1000*parseInt(boardTimestamp, 16));
+
+            html += '<div class="board" id="' + board.id + '" data-date="' + boardDate.getTime() + '">'
                 html += '<h2>' + board.name + '</h2>'
+
+                html += '<div class="content">'
+
+                    board.cards.forEach(card => {
+
+                        let cardTimestamp = card.id.substring(0,8)
+                        let cardDate = new Date(1000*parseInt(cardTimestamp, 16));
+
+                        html += '<div class="card" id="' + card.id + '" data-date="' + cardDate.getTime() + '">'
+                            html += '<h3><a href="' + card.url + '" target="_blank">' + card.name + '</a></h3>'
+                            html += '<div class="more-info">'
+                                html += '<ul>'
+                                    html += '<li>Created: ' + cardDate + '</li>'
+                                html += '</ul>'
+                            html += '</div>'
+                        html += '</div>'
+                    })
+
+                html += '</div>'
+
             html += '</div>'
         })
 
