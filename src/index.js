369a54e7081d8bdb76643c8ce088dbb3d4b49063
Unknown <liam.winterton.dev@gmail.com>
2018-04-12 16:04:40 +0100

    Now working with a hardcoded delay. Damn live servers

--- src/index.js
+++ src/index.js
@@ -10,45 +10,48 @@ jQuery(document).ready(function() {
 const trellowdown = new Trellowdown()
 
     trellowdown.auth().then(isAuthed => {
-        if(isAuthed) {
-            const userCards = Trellowdown.getUserCards()
-            const userID = Trellowdown.getUserID()
-            const OllyID = "5452114aee1bdab3526e47e1"
-
-            // Get all the users cards and the user ID, then
-            Promise.all([userCards, userID]).then(data => {
-                const id = data[1]
+        setTimeout(() => {
+            if(isAuthed) {
+                const userCards = Trellowdown.getUserCards()
+                const userID = Trellowdown.getUserID()
                 const OllyID = "5452114aee1bdab3526e47e1"
-
-                const addEvents = () => {
-                    TrelloCard.handleCardFlag(id)
-                }
-
-                let cards = data[0]
-
-                // Remove any unarchived cards and make sure all cards have user attached
-                cards = TrelloCard.filterByClosed(cards)
-                cards = TrelloCard.filterByUserID(cards, id)
-                cards = TrelloCard.filterByBoardBlacklist(cards)
-
-                // Setup the structure to be used in the App
-                let ids = TrelloCard.extractIdsFromCards(cards)
-                let emptyBoards = TrelloBoard.generateStructure(ids)
-
-                // Add in the cards to each board
-                let boards = TrelloBoard.addCards(emptyBoards, cards)
-                let finishedBoards = TrelloBoard.addBoardNames(boards)
-
-                // Once the list of boards is finished, sort and display mother flipper!
-                finishedBoards.then(boards => {
-                    let sorted = TrelloBoard.sortBoards(boards)
-                    jQuery("#app").html(Trellowdown.generateHTML(sorted))
-                    
-                    // Add some click events to buttons and such
-                    addEvents()
+    
+                // Get all the users cards and the user ID, then
+                Promise.all([userCards, userID]).then(data => {
+                    const id = data[1]
+                    const OllyID = "5452114aee1bdab3526e47e1"
+    
+                    const addEvents = () => {
+                        TrelloCard.handleCardFlag(id)
+                    }
+    
+                    let cards = data[0]
+    
+                    // Remove any unarchived cards and make sure all cards have user attached
+                    cards = TrelloCard.filterByClosed(cards)
+                    cards = TrelloCard.filterByUserID(cards, id)
+                    cards = TrelloCard.filterByBoardBlacklist(cards)
+    
+                    // Setup the structure to be used in the App
+                    let ids = TrelloCard.extractIdsFromCards(cards)
+                    let emptyBoards = TrelloBoard.generateStructure(ids)
+    
+                    // Add in the cards to each board
+                    let boards = TrelloBoard.addCards(emptyBoards, cards)
+                    let finishedBoards = TrelloBoard.addBoardNames(boards)
+    
+                    // Once the list of boards is finished, sort and display mother flipper!
+                    finishedBoards.then(boards => {
+                        let sorted = TrelloBoard.sortBoards(boards)
+                        jQuery("#app").html(Trellowdown.generateHTML(sorted))
+                        
+                        // Add some click events to buttons and such
+                        addEvents()
+                    })
                 })
-            })
-        }
+            }
+        }, 10000)
+        
     })
 
 })
