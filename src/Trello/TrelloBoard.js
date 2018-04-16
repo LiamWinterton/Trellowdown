d3a64bf16facf2116e8248c03ce0325eb1666212
Unknown <liam.winterton.dev@gmail.com>
2018-04-16 14:24:15 +0100

    Cards are now sorted in ascending date (oldest first)

--- src/Trello/TrelloBoard.js
+++ src/Trello/TrelloBoard.js
@@ -38,6 +38,19 @@ export class TrelloBoard {
             // Add each relevant card to the board
             let matches = cards.filter(card => card.idBoard == board.id)
 
+            // Sort matched cards by date and puuuuuuuuuuuush
+            matches = matches.sort((a, b) => {
+                let aCardTimestamp = a.id.substring(0, 8)
+                let aCardDate = new Date(1000 * parseInt(aCardTimestamp, 16));
+                let aTime = aCardDate.getTime()
+                
+                let bCardTimestamp = b.id.substring(0, 8)
+                let bCardDate = new Date(1000 * parseInt(bCardTimestamp, 16));
+                let bTime = bCardDate.getTime()
+    
+                aTime - bTime
+            })
+
             matches.forEach(card => {
                 board.cards.push(card)
             })
