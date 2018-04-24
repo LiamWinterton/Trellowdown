d6b412a5a79011ee87b1d1e6ba7588d69849b67e
dijitul_liam <liam.winterton.dev@gmail.com>
2018-04-24 14:30:19 +0100

    Boards added to select, as well as the initial lists. Need to get more on change

--- src/Trello/QuickAdd.js
+++ src/Trello/QuickAdd.js
@@ -1,23 +1,52 @@
 import { TrelloOrganisation } from './TrelloOrganisation'
+import { TrelloList } from './TrelloList'
 
 export class QuickAdd {
-    static setup() {
+    static setup(boards) {
         // Add boards and lists to selects
+        this.handleSelects(boards)
 
         // Add Members icons to organisation
-        TrelloOrganisation.getCurrentOrganisation()
-            .then(id => TrelloOrganisation.getMembers(id))
-            .then(members => {
-                console.log(members)
-            })
+        // TrelloOrganisation.getCurrentOrganisation()
+        //     .then(id => TrelloOrganisation.getMembers(id))
+        //     .then(members => {
+        //         console.log(members)
+        //     })
     }
 
     static handleSelects(boards) {
-        const boardSelect = jQuery("header #quick-add select[name='trello-boards']")
-        const listSelect = jQuery("header #quick-add select[name='trello-lists']")
+        const selector = "header #quick-add"
+        const boardSelect = jQuery(`${selector} select[name='trello-boards']`)
+        const listSelect = jQuery(`${selector} select[name='trello-lists']`)
+
+        boards.forEach(board => {
+            if(board.name !== "Priority") {
+                boardSelect.append(`<option value="${board.id}">${board.name}</option>`)
+            }
+        })
+
+        jQuery(`${selector} select option[value="0"]`).remove()
+
+        jQuery(boardSelect).change(this.onBoardSelectChange())
     }
 
     static handleButton() {
 
+    }
+
+    static onBoardSelectChange() {
+        const selector = "header #quick-add"
+        const boardSelect = jQuery(`${selector} select[name='trello-boards']`)
+        const boardID = boardSelect.val()
+
+        const lists = TrelloList.getListsFromBoard(boardID)
+            .then(lists => {
+                console.log(lists)
+
+                lists.forEach(list => {
+                    const listSelect = jQuery(`${selector} select[name='trello-lists']`)
+                    listSelect.append(`<option value="${list.id}">${list.name}</option>`)
+                })
+            })
     }
 }
