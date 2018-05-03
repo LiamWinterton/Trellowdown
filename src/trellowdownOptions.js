5dcc7e65c4aac5b47f48b64d9450541e76054823
dijitul_liam <liam.winterton.dev@gmail.com>
2018-05-03 16:13:20 +0100

    Added option to toggle quick add section

--- src/trellowdownOptions.js
+++ src/trellowdownOptions.js
@@ -4,16 +4,28 @@ export class TrellowdownOptions {
             TrellowdownOptions.setNavigation(jQuery(this).prop("checked"))
         })
 
+        jQuery("#quick-add-toggle").on("click", function(event) {
+            TrellowdownOptions.setQuickAdd(jQuery(this).prop("checked"))
+        })
+
         TrellowdownOptions.setDefaults()
     }
 
     static setDefaults() {
-        const preference = localStorage.getItem("td_navigation_toggle")
+        const navigationPreference = localStorage.getItem("td_navigation_toggle")
+        const quickAddPreference = localStorage.getItem("td_quick_add_toggle")
 
-        if(preference === "true") {
-            this.setNavigation(true)
+        this.setOption(this.setNavigation, navigationPreference, false)
+        this.setOption(this.setQuickAdd, quickAddPreference, true)
+    }
+
+    static setOption(callback, value, defaultValue) {
+        if(value === "true") {
+            callback(true)
+        } else if(value === "false") {
+            callback(false)
         } else {
-            this.setNavigation(false)
+            callback(defaultValue)
         }
     }
 
@@ -26,6 +38,18 @@ export class TrellowdownOptions {
             jQuery(".trellowdown-menu").hide()
             jQuery("#navigation-toggle").prop('checked', false)
             localStorage.setItem("td_navigation_toggle", false)
+        }
+    }
+
+    static setQuickAdd(value) {
+        if(value === true) {
+            jQuery("#quick-add").show()
+            jQuery("#quick-add-toggle").prop('checked', true)
+            localStorage.setItem("td_quick_add_toggle", true)
+        } else {
+            jQuery("#quick-add").hide()
+            jQuery("#quick-add-toggle").prop('checked', false)
+            localStorage.setItem("td_quick_add_toggle", false)
         }
     }
 }
